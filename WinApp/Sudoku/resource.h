55f31e9ce87a5105cc9553ccb682ae0be14420e2
T.Kusukawa <T.Kusukawa@a5333144-5879-11de-9579-adebd1cebf4d>
2009-06-28 00:39:26 +0000

    数独プログラムをコミット

---
+++ WinApp/Sudoku/resource.h
@@ -0,0 +1,101 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Sudoku.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_SUDOKU_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1_1                     1001
+#define IDC_EDIT1_2                     1002
+#define IDC_EDIT1_3                     1003
+#define IDC_EDIT2_1                     1004
+#define IDC_EDIT2_2                     1005
+#define IDC_EDIT2_3                     1006
+#define IDC_EDIT3_1                     1007
+#define IDC_EDIT3_2                     1008
+#define IDC_EDIT3_3                     1009
+#define IDC_EDIT1_4                     1030
+#define IDC_EDIT1_5                     1031
+#define IDC_EDIT1_6                     1032
+#define IDC_EDIT2_4                     1033
+#define IDC_EDIT2_5                     1034
+#define IDC_EDIT2_6                     1035
+#define IDC_EDIT3_4                     1036
+#define IDC_EDIT3_5                     1037
+#define IDC_EDIT3_6                     1038
+#define IDC_EDIT1_7                     1039
+#define IDC_EDIT1_8                     1040
+#define IDC_EDIT1_9                     1041
+#define IDC_EDIT2_7                     1042
+#define IDC_EDIT2_8                     1043
+#define IDC_EDIT2_9                     1044
+#define IDC_EDIT3_7                     1045
+#define IDC_EDIT3_8                     1046
+#define IDC_EDIT3_9                     1047
+#define IDC_EDIT4_1                     1048
+#define IDC_EDIT4_2                     1049
+#define IDC_EDIT4_3                     1050
+#define IDC_EDIT5_1                     1051
+#define IDC_EDIT5_2                     1052
+#define IDC_EDIT5_3                     1053
+#define IDC_EDIT6_1                     1054
+#define IDC_EDIT6_2                     1055
+#define IDC_EDIT6_3                     1056
+#define IDC_EDIT4_4                     1057
+#define IDC_EDIT4_5                     1058
+#define IDC_EDIT4_6                     1059
+#define IDC_EDIT5_4                     1060
+#define IDC_EDIT5_5                     1061
+#define IDC_EDIT5_6                     1062
+#define IDC_EDIT6_4                     1063
+#define IDC_EDIT6_5                     1064
+#define IDC_EDIT6_6                     1065
+#define IDC_EDIT4_7                     1066
+#define IDC_EDIT4_8                     1067
+#define IDC_EDIT4_9                     1068
+#define IDC_EDIT5_7                     1069
+#define IDC_EDIT5_8                     1070
+#define IDC_EDIT5_9                     1071
+#define IDC_EDIT6_7                     1072
+#define IDC_EDIT6_8                     1073
+#define IDC_EDIT6_9                     1074
+#define IDC_EDIT7_1                     1075
+#define IDC_EDIT7_2                     1076
+#define IDC_EDIT7_3                     1077
+#define IDC_EDIT8_1                     1078
+#define IDC_EDIT8_2                     1079
+#define IDC_EDIT8_3                     1080
+#define IDC_EDIT9_1                     1081
+#define IDC_EDIT9_2                     1082
+#define IDC_EDIT9_3                     1083
+#define IDC_EDIT7_4                     1084
+#define IDC_EDIT7_5                     1085
+#define IDC_EDIT7_6                     1086
+#define IDC_EDIT8_4                     1087
+#define IDC_EDIT8_5                     1088
+#define IDC_EDIT8_6                     1089
+#define IDC_EDIT9_4                     1090
+#define IDC_EDIT9_5                     1091
+#define IDC_EDIT9_6                     1092
+#define IDC_EDIT7_7                     1093
+#define IDC_EDIT7_8                     1094
+#define IDC_EDIT7_9                     1095
+#define IDC_EDIT8_7                     1096
+#define IDC_EDIT8_8                     1097
+#define IDC_EDIT8_9                     1098
+#define IDC_EDIT9_7                     1099
+#define IDC_EDIT9_8                     1100
+#define IDC_EDIT9_9                     1101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
