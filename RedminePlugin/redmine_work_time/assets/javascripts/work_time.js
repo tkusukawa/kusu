b60b7fde0bddea861ecdaad6afd3f10f7b755371
T.Kusukawa <T.Kusukawa@a5333144-5879-11de-9579-adebd1cebf4d>
2013-02-16 03:59:16 +0000

    Version 0.2.7 erbに散らばっていたJSをwork_time.jsに収集 AタグのJS表記を統一 Redmine2.0と2.1の両方で使えるように修正 "data download"のラベルの追加忘れを修正 add data export function チケットが無くなっていた時の例外処理を追加 init.rbにプロジェクトURLを記述 日毎メモの更新時間の表示がおかしかったので修正 集計表示の不具合修正と効率の改善 集計表示を小数点以下2桁で右寄せで表示するように変更

--- RedminePlugin/redmine_work_time/assets/javascripts/work_time.js
+++ RedminePlugin/redmine_work_time/assets/javascripts/work_time.js
@@ -35,13 +35,20 @@ function set_ticket_relay(pop_url, rep_url, child)
 {
   var parent = showModalDialog(pop_url, window, "dialogWidth:600px;dialogHeight:480px");
   if(parent!=null) {
-    jQuery.ajax({
-      url:rep_url+"&ticket_relay="+child+"_"+parent,
-      data:{asynchronous:true, method:'get'},
-      success:function(response){
-        jQuery('#ticket'+child).html(response);
-      }
-    });
+    if( typeof jQuery == "function" ) {
+      jQuery.ajax({
+        url:rep_url+"&ticket_relay="+child+"_"+parent,
+        data:{asynchronous:true, method:'get'},
+        success:function(response){
+          jQuery('#ticket'+child).html(response);
+        }
+      });
+    }
+    else {
+      new Ajax.Updater('ticket'+child,
+        rep_url+"&ticket_relay="+child+"_"+parent,
+        {asynchronous:true, method:'get'});
+    }
   }
 }
 
@@ -50,18 +57,179 @@ function update_done_ratio(pop_url, rep_url, issue_id)
   var done_ratio = showModalDialog(pop_url+"&issue_id="+issue_id,
         window, "dialogWidth:500px;dialogHeight:150px");
   if(done_ratio!=null){
-    jQuery.ajax({
-      url:rep_url+"&issue_id="+issue_id+"&done_ratio="+done_ratio,
-      data:{asynchronous:true, method:'get'},
-      success:function(response){
-        jQuery('#done_ratio'+issue_id).html(response);
-      }
-    });
+    if( typeof jQuery == "function" ) {
+      jQuery.ajax({
+        url:rep_url+"&issue_id="+issue_id+"&done_ratio="+done_ratio,
+        data:{asynchronous:true, method:'get'},
+        success:function(response){
+          jQuery('#done_ratio'+issue_id).html(response);
+        }
+      });
+    }
+    else {
+      new Ajax.Updater('done_ratio'+issue_id,
+        rep_url+"&issue_id="+issue_id+"&done_ratio="+done_ratio,
+        {asynchronous:true, method:'get'});
+    }
 
     var drs = document.getElementsByName("done_ratio"+issue_id);
     for(var i = 0; i < drs.length; i++) {
       drs[i].innerHTML = "["+done_ratio+"&#37;]";
     }
   }
+}
 
+function del_ticket_relay(rep_url, child)
+{
+  if( typeof jQuery == "function" ) {
+    jQuery.ajax({
+        url:rep_url+"&ticket_relay="+child+"_0",
+        data:{asynchronous:true, method:'get'
+      },
+      success:function(response){
+        jQuery('#ticket'+child).html(response);
+      }
+    });
+  }
+  else {
+    new Ajax.Updater('ticket'+child,
+      rep_url+"&ticket_relay="+child+"_0",
+      {asynchronous:true, method:'get'});
+  }
+}
+
+function checkKey(e, finish_func)
+{
+  if (!e) var e = window.event;
+  if(e.keyCode == 13) {
+    finish_func();
+    return false;
+  }
+  else
+    return e;
+}
+
+function ajax_select_tickets(rep_url)
+{
+  if( typeof jQuery == "function" ) {
+    jQuery.ajax({
+      url:rep_url,
+      data:{asynchronous:true, method:'get'},
+      success:function(response){
+        jQuery('#tickets').html(response);
+      }
+    });
+  }
+  else {
+    new Ajax.Updater('tickets',
+      rep_url,
+      {asynchronous:true, method:'get'});
+  }
+}
+
+//------------------------------------------------- for show.html.erb
+var add_ticket_count = 1;
+function add_ticket(pop_url, ajax_url)
+{
+    var tickets = showModalDialog(pop_url, window, "dialogWidth:600px;dialogHeight:480px");
+    for(i=0; i<tickets.length;i++) {
+      if( typeof jQuery == "function" ) {
+        jQuery.ajax({
+          url:ajax_url+"&add_issue="+tickets[i]+"&count="+add_ticket_count,
+          data:{asynchronous:true, method:'get'},
+          success:function(response){
+            jQuery('#time_input_table_bottom').before(response);
+          }
+        });
+      }
+      else {
+        new Ajax.Updater('time_input_table_bottom',
+          ajax_url+"&add_issue="+tickets[i]+"&count="+add_ticket_count,
+          {insertion:Insertion.Before, method:'get'});
+      }
+      add_ticket_count ++;
+    }
+}
+
+function dup_ticket(ajax_url, insert_pos, id)
+{
+  if( typeof jQuery == "function" ) {
+    jQuery.ajax({
+      url:ajax_url+"&add_issue="+id+"&count="+add_ticket_count,
+      data:{asynchronous:true, method:'get'},
+      success:function(response){
+        jQuery('#'+insert_pos).after(response);
+      }
+    });
+  }
+  else {
+    new Ajax.Updater( insert_pos,
+      ajax_url+"&add_issue="+id+"&count="+add_ticket_count,
+      {insertion:Insertion.After, method:'get'});
+  }
+  add_ticket_count ++;
+}
+
+function edit_memo(ajax_url)
+{
+  if( typeof jQuery == "function" ) {
+    jQuery.ajax({
+      url:ajax_url,
+      data:{asynchronous:true, method:'get'},
+      success: function(response){
+        jQuery('#memo-wiki').html(response);
+      }
+    });
+  }
+  else {
+    new Ajax.Updater('memo-wiki',
+      ajax_url,
+      {asynchronous:true, method:'get'});
+  }
+}
+
+//---------------------------------------- for popup_update_done_ratio.html.erb
+function ratio_inputed()
+{
+  returnValue = document.getElementById("input_ratio").value;
+  close();
+}
+
+//--------------- for popup_select_ticket.html.erb, ajax_select_ticket.html.erb
+function ticket_inputed()
+{
+  returnValue = document.getElementById("input_id").value;
+  close();
+}
+
+function ticket_selected(issue_id)
+{
+  returnValue = issue_id;
+  close();
+}
+
+//------------- for popup_select_tickets.html.erb, ajax_select_tickets.html.erb
+function tickets_inputed()
+{
+  var vals = document.getElementById("input_ids").value;
+  var tickets = vals.split(',');
+  returnValue=tickets;
+  close();
+}
+
+function tickets_selected(issue_id)
+{
+  returnValue = [issue_id];
+  close();
+}
+
+function tickets_checked()
+{
+  var issue_ids = new Array;
+  for(i=0; e = document.forms[0].elements[i]; i++) {
+    if(e.checked)
+      issue_ids.push(e.value);
+  }
+  returnValue = issue_ids;
+  close();
 }
