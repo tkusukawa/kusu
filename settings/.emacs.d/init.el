29c948de6917ff3ea494e7a1867f5aa8fd6d1cf8
T.Kusukawa@gmail.com <T.Kusukawa@gmail.com@a5333144-5879-11de-9579-adebd1cebf4d>
2014-12-10 07:49:25 +0000

    care shell mode

--- settings/.emacs.d/init.el
+++ settings/.emacs.d/init.el
@@ -1,6 +1,14 @@
 (define-key global-map "\C-z" 'undo)
 (define-key global-map "\C-q" 'kill-ring-save)
-(define-key global-map "\C-i" 'isearch-forward)
-(define-key global-map "\C-s" 'save-buffer)
+;(define-key global-map "\C-i" 'isearch-forward)
+;(define-key global-map "\C-s" 'save-buffer)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(add-hook 'shell-mode-hook
+          '(lambda ()
+             (progn
+               ;; (define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
+               ;; (define-key shell-mode-map (kbd "C-n") 'comint-next-input)
+               (define-key shell-mode-map (kbd "C-p") 'comint-previous-matching-input-from-input)
+               (define-key shell-mode-map (kbd "C-n") 'comint-next-matching-input-from-input)
+               )))
